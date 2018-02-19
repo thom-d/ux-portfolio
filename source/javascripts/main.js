8520c6bc1f46618b599b5c5b8678eafa72b3c232
Dana Thomas <d.thomas014@gmail.com>
2018-02-18 19:52:29 -0500

    initial commit

---
+++ source/javascripts/main.js
@@ -0,0 +1,55 @@
+// This is where it all goes :)
+
+
+
+$(document).ready(function() {
+
+// Introduction text/cta - fades in during page load. 
+
+$(".landing-intro, .button").hide().fadeIn("slow");
+
+
+// "Featured Work" and "About" links 
+// On click scrolls down to portfolio gallery and about section
+
+$(".my-gallery").click(function(e){
+  e.preventDefault();
+  $('body, html').animate({scrollTop: $(".project").offset().top}, 1500);    
+});
+
+$(".my-blurb").click(function(e){
+  e.preventDefault();
+  $('body, html').animate({scrollTop: $("footer").offset().top}, 1500);    
+});
+
+// Call-To-Action
+// On click scrolls down to portfolio gallery
+
+$(".button").click(function(e){
+  e.preventDefault();
+  $('body, html').animate({scrollTop: $(".project").offset().top}, 1500);    
+});
+
+
+// Gallery Card
+// On hover, project title transitions to unique color
+
+// var grid = ".card-title";
+
+// $( ".card" ).hover (function() {
+// 		$( this ).children(".card-title").css( "background-color", "#2e2e2e" );
+//   });
+// $( ".card" ).mouseleave (function() {
+// 		$( this ).children(".card-title").css( "background-color", "#363636" );
+//   });
+
+
+// Return link/button
+// On click scrolls up to page header
+
+$(".return").click(function(e){
+  e.preventDefault();
+  $('body, html').animate({scrollTop: $(".landing").offset().top}, 1500);    
+});  
+
+});
